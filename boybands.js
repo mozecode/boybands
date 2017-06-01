281033311bcefde787317518b147961ee4f935be
Greta Moseley <greta.moseley@gmail.com>
2017-05-31 20:43:31 -0500

    boybands js complete

--- boybands.js
+++ boybands.js
@@ -11,25 +11,40 @@ var currentBand = "";
 // Keep track of which veggie we're on in the loop
 var currentVeggie = "";
 
-function printItemToDiv(message) {
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById("boy-bands");
-bandElement.innerHTML = message;
-
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = document.getElementById("vegetables");
-veggieElement.innerHTML = message;
-
+function printBandToDiv(message){
+	// Get a reference to the appropriate DOM element for bands
+	var bandElement = document.getElementById("boy-bands");
+	bandElement.innerHTML = message;
 }
 
+function printVeggieToDiv(vegmessage){
+// Get a reference to the appropriate DOM element for veg
+	var veggieElement = document.getElementById("vegetables");
+	veggieElement.innerHTML = vegmessage;
+}
+
+function iterateThroughArray(arr){
 // Start looping
-for (var loopTracker = 0; loopTracker < vegetables.length; loopTracker += 1) {
+	for (var loopTracker = 0; loopTracker < bands.length; loopTracker += 1) {
   
-  // Add the band names into the correct <div>
-  currentBand = bands[loopTracker];
-
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = vegetables[loopTracker];
-
+ 	 // Add the band names into the correct <div>
+  		currentBand += `<div>${bands[loopTracker]}</div>`;
+  		
+	}
+ 	
+ 	printBandToDiv(currentBand);
+ 
 }
+
+function iterateThroughNextArray(vegarray){
+
+	for (var loopTracker = 0; loopTracker < vegetables.length; loopTracker += 1){
+
+ 	currentVeggie += `<div>${vegetables[loopTracker]}</div>`;
+	}
+
+	printVeggieToDiv(currentVeggie);
+}
+
+ iterateThroughArray(bands);
+ iterateThroughNextArray(vegetables);
